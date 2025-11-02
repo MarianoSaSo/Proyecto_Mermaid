5c341be66f5acde93b30b4cbc811cce35efc00c6
Mariano SaSo <152398164+Cuco89@users.noreply.github.com>
2025-11-02 22:07:17 +0100

    Changes on superior nadbar paths

--- frontend/src/components/main-nav.tsx
+++ frontend/src/components/main-nav.tsx
@@ -10,7 +10,7 @@ export function MainNav() {
 
   const items = [
     {
-      href: '/',
+      href: '/Home',
       label: 'Inicio',
     },
     {
@@ -47,7 +47,7 @@ export function MainNav() {
         </div>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <div className="w-full flex-1 md:w-auto md:flex-none">
-            {/* Aquí podrías agregar una barra de búsqueda , mas adelante lo haremos*/}
+            {/* Aquí podríamos agregar una barra de búsqueda , mas adelante lo haremos*/}
           </div>
           <nav className="flex items-center">
             <ModeToggle />
